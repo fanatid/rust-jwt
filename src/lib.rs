f3c10beed0c750e800bbedec4b4fca8e85463edd
Kirill Fomichev <fanatid@ya.ru>
2020-06-29 10:55:11 +0300

    Remove extra to_owned calls

--- src/lib.rs
+++ src/lib.rs
@@ -152,15 +152,17 @@ where
     }
 
     fn encode(param: &T) -> Result<String, JwtErr> {
-        Ok(encode_config(serde_json::to_string(&param)?.as_bytes(), base64::URL_SAFE).to_owned())
+        Ok(encode_config(
+            serde_json::to_string(&param)?.as_bytes(),
+            base64::URL_SAFE,
+        ))
     }
 
     fn encode_header(&self) -> Result<String, JwtErr> {
         Ok(encode_config(
             serde_json::to_string(&self.header()?)?.as_bytes(),
             base64::URL_SAFE,
-        )
-        .to_owned())
+        ))
     }
 
     fn header(&self) -> Result<JwtHeader, JwtErr> {
